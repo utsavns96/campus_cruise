f3b9ee98ea32280167ec323e419ca0a362689895
Hrishikesh Badve <112002709+HrishikeshBadve@users.noreply.github.com>
2023-11-08 18:30:07 -0600

    Alert Screen

--- lib/alerts.dart
+++ lib/alerts.dart
@@ -80,6 +80,126 @@ class _basicState extends State<AlertScreenPage> {
                 ),
               ),
             ),
+            Center(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget> [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget> [
+                          Container(
+                              //padding: EdgeInsets.only(top: 100),
+                              width:350,
+                              height:30,
+                              color: Colors.amber,
+                              child: Center(
+                                child: const Text(
+                                  "ALERT",
+                                  style: TextStyle(
+                                    fontFamily: 'Raleway',
+                                    fontSize: 25.0,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                          ),
+                          Container(
+                            width:350,
+                            height:125,
+                            color: Colors.black,
+                            child: Center(
+                              child: const Text(
+                                "Night Ride Service Timings Change: Starting Fall 2023, NightRide Service will be available from 10:00 pm to 7:00 am on all 7 days of the week ",
+                                style: TextStyle(
+                                  fontFamily: 'Raleway',
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            )
+                          )
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget> [
+                          Container(
+                              width:350,
+                              height:30,
+                              color: Colors.red,
+                              child: Center(
+                                child: const Text(
+                                  "WARNING",
+                                  style: TextStyle(
+                                    fontFamily: 'Raleway',
+                                    fontSize: 25.0,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  )
+                                ),
+                              )
+                          ),
+                          Container(
+                              width:350,
+                              height:125,
+                              color: Colors.black,
+                              child: Center(
+                                child: const Text(
+                                  "Night Ride Services are currently disrupted from Taylor & Racine to Taylor & Ada streets due to roadwork. Riders are requested to take this into account while booking rides",
+                                  style: TextStyle(
+                                    fontFamily: 'Raleway',
+                                    fontSize: 15.0,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              )
+                          )
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget> [
+                          Container(
+                              width:350,
+                              height:30,
+                              color: Colors.deepPurpleAccent,
+                              child: Center(
+                                child: const Text(
+                                    "NOTICE",
+                                    style: TextStyle(
+                                      fontFamily: 'Raleway',
+                                      fontSize: 25.0,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    )
+                                ),
+                              )
+                          ),
+                          Container(
+                              width:350,
+                              height:125,
+                              color: Colors.black,
+                              child: Center(
+                                child: const Text(
+                                  "Night Ride 7 is currently unavailable due to maintenance. Wait times may be higher due to this.",
+                                  style: TextStyle(
+                                    fontFamily: 'Raleway',
+                                    fontSize: 15.0,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              )
+                          )
+                        ],
+                      )
+                    ]
+                )
+            )
           ],
         )
     );
