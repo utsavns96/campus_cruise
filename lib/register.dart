2796658f00d0391fecfa55ed154e946d356835ff
Hrishikesh Badve <112002709+HrishikeshBadve@users.noreply.github.com>
2023-11-09 16:06:04 -0600

    Registration and registration complete pages

--- lib/register.dart
+++ lib/register.dart
@@ -1,3 +1,5 @@
+import 'package:campus_cruise/login.dart';
+import 'package:campus_cruise/registrationsuccessful.dart';
 import 'package:flutter/material.dart';
 
 void registerpage() => runApp(MaterialApp(
@@ -80,6 +82,85 @@ class _basicState extends State<RegisterScreenPage> {
                 ),
               ),
             ),
+            Column(
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.only(top: 100),
+                  child: Image.asset('assets/logo.png')),
+                    Container(
+                      padding: const EdgeInsets.only(top: 10, bottom: 50),
+                      child: const Text(
+                        "Register:",
+                        style: TextStyle(
+                        fontFamily: 'Raleway',
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        letterSpacing: 8.0,
+                      ),
+                    ),
+                  ),
+                Container(
+                  padding: const EdgeInsets.all(10.0),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Enter your Email ID',
+                      hintText: 'Enter valid mail id as abc@uic.edu',
+                      filled: true,
+                      fillColor: Colors.white,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(10.0),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Enter your Password',
+                      hintText: 'Enter password',
+                      filled: true,
+                      fillColor: Colors.white,
+                    ),
+                    obscureText: true,
+                    obscuringCharacter: '*',
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(10.0),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Re-confirm your Password',
+                      hintText: 'Enter password again',
+                      filled: true,
+                      fillColor: Colors.white,
+                    ),
+                    obscureText: true,
+                    obscuringCharacter: '*',
+                  ),
+                ),
+                Container(
+                    padding: const EdgeInsets.only(top: 50),
+                    width: 300,
+                    height: 100,
+                    child: FloatingActionButton(
+                      onPressed: () {
+                        //TO DO: Add login functionality
+                        Navigator.push(context, MaterialPageRoute(builder: (_) => const RegistrationSuccessfulScreenPage(title: 'Campus Cruise')));
+                      },
+                      backgroundColor: Colors.lightBlue,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                      child: const Text('Register',
+                          style: TextStyle(
+                            fontFamily: 'Raleway',
+                            fontSize: 25.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          )),
+                    ))
+                ],
+            )
           ],
         )
     );
---
+++ lib/registrationsuccessful.dart
@@ -0,0 +1,142 @@
+import 'package:campus_cruise/login.dart';
+import 'package:flutter/material.dart';
+
+void registrationsuccessfulpage() => runApp(MaterialApp(
+  title: 'Campus Cruise',
+  home: registrationsuccessfulscreen(),
+));
+
+class registrationsuccessfulscreen extends StatefulWidget {
+  const registrationsuccessfulscreen({super.key});
+
+  @override
+  State<registrationsuccessfulscreen> createState() => _registrationsuccessfulscreenState();
+}
+
+class _registrationsuccessfulscreenState extends State<registrationsuccessfulscreen> {
+  @override
+  void initState(){
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.push(context, MaterialPageRoute(builder: (_) => const Login()));
+    });
+  }
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        title: Text(
+          'Campus Cruise',
+          style: TextStyle(
+            fontFamily: 'Raleway',
+            fontSize: 25.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Color(0x00000000),
+        elevation: 0.0,
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/bgimage.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RegistrationSuccessfulScreenPage extends StatefulWidget {
+  const RegistrationSuccessfulScreenPage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<RegistrationSuccessfulScreenPage> createState() => _basicState();
+}
+
+class _basicState extends State<RegistrationSuccessfulScreenPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          title: const Text(
+            'Campus Cruise',
+            style: TextStyle(
+              fontFamily: 'Raleway',
+              fontSize: 25.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: Color(0x00000000),
+          elevation: 0.0,
+        ),
+        body: Stack(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: const AssetImage("assets/bgimage.png"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center, // This should center your Column's children
+                  children: <Widget>[
+                  Container(
+                    child: const Icon(
+                      Icons.check_circle_outline, // This is the tick mark icon
+                      size: 100.0, // Size of the tick mark
+                      color: Colors.green, // Color for the tick mark
+                    ),
+                  ),
+                  Container(
+                    child: const Text(
+                      'Registration',
+                      style: TextStyle(
+                        fontFamily: 'Raleway',
+                        fontSize: 40.0, // Larger text size
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                    Container(
+                      child: const Text(
+                        'Successful',
+                        style: TextStyle(
+                          fontFamily: 'Raleway',
+                          fontSize: 40.0, // Larger text size
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      child: const Text(
+                        'Thank you for registering!',
+                        style: TextStyle(
+                          fontFamily: 'Raleway',
+                          fontSize: 20.0, // Smaller text size
+                          fontWeight: FontWeight.normal,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+              ),
+            ),
+          ]
+        ),
+    );
+  }
+}
+
